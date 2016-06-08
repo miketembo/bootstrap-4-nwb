96e22447a544a3d78658e5eff65d2141ecf373cb
Jonny Buchanan <jonathan.buchanan@gmail.com>
2016-06-08 23:59:26 +1000

    Add a Blog component and use our local Bootstrap variables in its stylesheet
    HTML and CSS were copied from the Bootstrap 4 blog example:
    http://v4-alpha.getbootstrap.com/examples/blog/
    
    CSS tweaks:
    - Rules for blog styling were nested in a .blog rule
    - The hardcoded background colour in .blog-masthead was replaced with
    $brand-primary from our local Bootstrap variables.
    
    The HTML needed a couple of tweaks to be usable:
    - Closing an <hr> (JSX elements must be closed)
    - Removing some <!-- --> comments (JSX uses {/* */} for comments)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './bootstrap.scss'
 
 import React from 'react'
 
+import Blog from './Blog'
+
 export default React.createClass({
   render() {
     return <div>
@@ -9,6 +11,8 @@ export default React.createClass({
         <h2>Welcome to React</h2>
         <button class="btn btn-primary">Button</button>
       </div>
+      <hr/>
+      <Blog/>
     </div>
   }
 })
